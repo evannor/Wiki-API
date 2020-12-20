78df2f1795260b21a43e9a7571e79da52346f231
evannor <erica.vannorman@gmail.com>
2020-12-20 15:00:52 -0800

    Create chained Express route handler

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ const articleSchema = {
 
 const Article = mongoose.model('Article', articleSchema);
 
-app.get('/articles', function (req, res) {
+app.route("/articles")
+
+.get(function (req, res) {
   Article.find(function (err, foundArticles) {
     if (!err) {
       res.send(foundArticles);
@@ -31,9 +33,9 @@ app.get('/articles', function (req, res) {
       res.send(err);
     }
   });
-});
+})
 
-app.post('/articles', function(req, res) {
+.post(function(req, res) {
 
   const newArticle = new Article({
     title: req.body.title,
@@ -47,9 +49,9 @@ app.post('/articles', function(req, res) {
       res.send(err);
     }
   });
-});
+})
 
-app.delete('/articles', function(req, res) {
+.delete(function(req, res) {
   Article.deleteMany(function(err,) {
     if(!err) {
       res.send('Successfully deleted all articles.');
