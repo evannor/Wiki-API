3219f602e2ebcaf5c9676926587c52bf82a7bceb
evannor <erica.vannorman@gmail.com>
2020-12-20 14:39:56 -0800

    Create delete route for all articles

--- app.js
+++ app.js
@@ -49,6 +49,16 @@ app.post('/articles', function(req, res) {
   });
 });
 
+app.delete('/articles', function(req, res) {
+  Article.deleteMany(function(err,) {
+    if(!err) {
+      res.send('Successfully deleted all articles.');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("Server is running on port 3000");
 });
